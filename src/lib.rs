a289bf36e12a10c50f0a164c77d7afc3a3757ab5
Felipe Lima <felipe@ferreiradelima.com>
2026-05-20 15:32:10 -0400

    chore(crate): scaffold cargo manifest and lib.rs

---
+++ src/lib.rs
@@ -0,0 +1,12 @@
+//! LoRaWAN 1.0/1.1 packet decoder and encoder.
+//!
+//! See the crate `README` for a quickstart.
+
+#![cfg_attr(not(feature = "std"), no_std)]
+#![deny(unsafe_code)]
+#![deny(missing_docs)]
+#![warn(clippy::pedantic)]
+#![warn(clippy::nursery)]
+#![allow(clippy::module_name_repetitions, clippy::must_use_candidate)]
+
+extern crate alloc;
